e23894fd488740382d245986e61e3c0a77af3171
Stefan79-afk <epsilon.number79@gmail.com>
2024-08-04 14:38:19 +0300

    add basic operations like 1/x, sqrt(x), x^2 + CE and C

--- src/app/calculator-output/calculator-output.component.ts
+++ src/app/calculator-output/calculator-output.component.ts
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 export class CalculatorOutputComponent {
   @Input() value: string = "";
+  @Input() history: string = "";
 }
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -12,8 +12,52 @@ import { CalculatorOutputComponent } from "../calculator-output/calculator-outpu
 export class CalculatorComponent {
   values = ["%", "CE", "C", "<x", "1/x", "x^2", "sqrt(x)", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="];
 
-  outputValue = '';
+  outputValue = "";
+  historyValue = ""
+
   onButtonPressReceive(value: string) {
-    this.outputValue = value;
+    switch(value) {
+
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if(this.outputValue) {
+          this.outputValue += value;
+        } else {
+          this.outputValue = value;
+        }
+        break;
+      
+      case "1/x":
+        if(this.outputValue) {
+          this.historyValue = `1/${this.outputValue}`;
+          this.outputValue = (1 / Number(this.outputValue)).toString();
+        }
+        break;
+      
+      case "sqrt(x)":
+        if(this.outputValue) {
+          this.historyValue = `sqrt(${this.outputValue})`;
+          this.outputValue = (Math.sqrt(Number(this.outputValue))).toString();
+        }
+        break;
+
+      case "x^2":
+        if(this.outputValue) {
+          this.historyValue = `${this.outputValue} ^ 2`;
+          this.outputValue = (Math.pow(Number(this.outputValue), 2)).toString();
+        }
+        break;
+
+      default:
+        this.outputValue = "";
+        this.historyValue = "";
+    }
   }
 }
